db549e6c8e00a5aa299f968faa62a0f3db0257aa
danutdotrar <morarasu.danut@gmail.com>
2024-02-27 18:50:21 +0200

    Add part1 ex 1.2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -15,9 +15,7 @@ function App() {
         <>
             <div>
                 <Header course={course} />
-                <Content part={part1} exercise={exercises1} />
-                <Content part={part2} exercise={exercises2} />
-                <Content part={part3} exercise={exercises3} />
+                <Content />
                 <Total exercises={exercises1 + exercises2 + exercises3} />
             </div>
         </>
--- part1/courseinfo/src/components/Content.jsx
+++ part1/courseinfo/src/components/Content.jsx
@@ -1,11 +1,19 @@
 import React from "react";
+import Part from "./Part";
 
-const Content = ({ part, exercise }) => {
+const part1 = "Fundamentals of React";
+const exercises1 = 10;
+const part2 = "Using props to pass data";
+const exercises2 = 7;
+const part3 = "State of a component";
+const exercises3 = 14;
+
+const Content = () => {
     return (
         <div>
-            <p>
-                {part} {exercise}
-            </p>
+            <Part part={part1} exercise={exercises1} />
+            <Part part={part2} exercise={exercises2} />
+            <Part part={part3} exercise={exercises3} />
         </div>
     );
 };
