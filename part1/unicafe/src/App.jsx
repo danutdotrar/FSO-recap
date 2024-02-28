fc6c255a07a09fac770365940639a2174ba2202f
danutdotrar <morarasu.danut@gmail.com>
2024-02-28 15:03:12 +0200

    Add part1 unicafe ex1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,21 +5,51 @@ function App() {
     const [good, setGood] = useState(0);
     const [neutral, setNeutral] = useState(0);
     const [bad, setBad] = useState(0);
+    const [total, setTotal] = useState(0);
+    const [average, setAverage] = useState([]);
+    const [positiveFeedback, setPositiveFeedback] = useState(0);
 
     const handleGood = () => {
         const goodCount = good + 1;
         setGood(goodCount);
+
+        const totalCounts = goodCount + neutral + bad;
+        setTotal(totalCounts);
+
+        setAverage(average.concat(1));
+
+        // set positive feedback
+        const goodFeedback = goodCount;
+        setPositiveFeedback(goodFeedback);
     };
 
     const handleNeutral = () => {
         const neutralCount = neutral + 1;
         setNeutral(neutralCount);
+
+        const totalCounts = good + neutralCount + bad;
+        setTotal(totalCounts);
+
+        setAverage(average.concat(0));
     };
 
     const handleBad = () => {
         const badCount = bad + 1;
         setBad(badCount);
+
+        const totalCounts = good + neutral + badCount;
+        setTotal(totalCounts);
+
+        setAverage(average.concat(-1));
     };
+
+    const allAverage =
+        average.reduce((acc, val) => acc + val, 0) / average.length;
+
+    // find percentage of the positive feedback (cati la % au votat good)
+    // aflam cati au votat good
+    // impartim nr de votanti good la nr total si inmultim cu 100 pt %
+    const goodPercentage = (positiveFeedback / total) * 100;
 
     return (
         <>
@@ -32,6 +62,9 @@ function App() {
             <div>good: {good}</div>
             <div>neutral:{neutral}</div>
             <div>bad: {bad}</div>
+            <div>all: {total}</div>
+            <div>average: {allAverage || 0}</div>
+            <div>positive: {goodPercentage || 0}</div>
         </>
     );
 }
