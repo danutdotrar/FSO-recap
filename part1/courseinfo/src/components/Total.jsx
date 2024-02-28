d0e4edab02e4a0080cd821a626d609a30a4626fc
danutdotrar <morarasu.danut@gmail.com>
2024-02-28 11:29:26 +0200

    Add part1 ex 1.4

--- part1/courseinfo/src/components/Total.jsx
+++ part1/courseinfo/src/components/Total.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 
-const Total = ({ exercises }) => {
+const Total = ({ parts }) => {
+    const sum = parts.reduce((acc, val) => acc + val.exercises, 0);
+
+    const sumBasic =
+        parts[0].exercises + parts[1].exercises + parts[2].exercises;
+
     return (
         <div>
             {" "}
-            <p>Number of exercises {exercises}</p>
+            <p>Number of exercises {sum}</p>
         </div>
     );
 };
