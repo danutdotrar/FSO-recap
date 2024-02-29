116f0c88cebb92aa646d4a3428d621de398bcaae
danutdotrar <morarasu.danut@gmail.com>
2024-02-29 20:21:20 +0200

    Add filter display elements

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,52 +1,66 @@
-import Course from "./components/Course";
+import { useState } from "react";
+import Note from "./components/Note";
 
-const App = () => {
-    const courses = [
-        {
-            name: "Half Stack application development",
-            id: 1,
-            parts: [
-                {
-                    name: "Fundamentals of React",
-                    exercises: 10,
-                    id: 1,
-                },
-                {
-                    name: "Using props to pass data",
-                    exercises: 7,
-                    id: 2,
-                },
-                {
-                    name: "State of a component",
-                    exercises: 14,
-                    id: 3,
-                },
-                {
-                    name: "Redux",
-                    exercises: 11,
-                    id: 4,
-                },
-            ],
-        },
-        {
-            name: "Node.js",
-            id: 2,
-            parts: [
-                {
-                    name: "Routing",
-                    exercises: 3,
-                    id: 1,
-                },
-                {
-                    name: "Middlewares",
-                    exercises: 7,
-                    id: 2,
-                },
-            ],
-        },
-    ];
+const App = (props) => {
+    const [notes, setNotes] = useState(props.notes);
+    const [newNote, setNewNote] = useState("");
+    const [showAll, setShowAll] = useState(true);
 
-    return <Course courses={courses} />;
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        // Create new noteObject
+        const noteObject = {
+            content: newNote,
+            important: Math.random() < 0.5,
+            id: notes.length + 1,
+        };
+
+        // Concat noteObject to notes state
+        setNotes(notes.concat(noteObject));
+
+        // reset newNote value
+        setNewNote("");
+    };
+
+    const handleInputChange = (e) => {
+        setNewNote(e.target.value);
+    };
+
+    const notesToShow = showAll
+        ? notes
+        : notes.filter((note) => note.important === true);
+
+    return (
+        <div>
+            <h1>Notes</h1>
+            <div>
+                <button onClick={() => setShowAll(!showAll)}>
+                    show {showAll ? "important" : "all"}
+                </button>
+            </div>
+
+            <ul>
+                {notesToShow.map((note) => (
+                    <Note key={note.id} note={note} />
+                ))}
+            </ul>
+
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <input
+                        type="text"
+                        value={newNote}
+                        onChange={handleInputChange}
+                    />
+                </div>
+
+                <div>
+                    <button type="submit">save note</button>
+                </div>
+            </form>
+        </div>
+    );
 };
 
 export default App;
