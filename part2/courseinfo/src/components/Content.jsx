7317c2b6e8ecc99acf1d22ed78dde0ee06c249ec
danutdotrar <morarasu.danut@gmail.com>
2024-02-29 14:07:18 +0200

    Add part2 courseinfo ex2.2

--- part2/courseinfo/src/components/Content.jsx
+++ part2/courseinfo/src/components/Content.jsx
@@ -2,11 +2,17 @@ import React from "react";
 import Part from "./Part";
 
 const Content = ({ course }) => {
+    const sumExercises = course.parts.reduce(
+        (acc, val) => acc + val.exercises,
+        0
+    );
+
     return (
         <div>
             {course.parts.map((part) => (
-                <Part part={part} />
+                <Part key={part.id} part={part} />
             ))}
+            <h3>{`total of ${sumExercises} exercises`}</h3>
         </div>
     );
 };
