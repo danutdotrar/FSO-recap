0718fe4dd2fa7e6690bd8da0d6a30aaa90c02a99
danutdotrar <morarasu.danut@gmail.com>
2024-03-01 21:17:34 +0200

    Add jsonserver

--- part2/courseinfo/src/main.jsx
+++ part2/courseinfo/src/main.jsx
@@ -1,25 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import axios from "axios";
 
-const notes = [
-    {
-        id: 1,
-        content: "HTML is easy",
-        important: true,
-    },
-    {
-        id: 2,
-        content: "Browser can execute only JavaScript",
-        important: false,
-    },
-    {
-        id: 3,
-        content: "GET and POST are the most important methods of HTTP protocol",
-        important: true,
-    },
-];
+// get the notes from the API with axios
+// axios.get() returns a promise so we chain .then to it to catch it
+axios.get("http://localhost:3001/notes").then((response) => {
+    // return data from the response obj
+    const notes = response.data;
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-    <App notes={notes} />
-);
+    ReactDOM.createRoot(document.getElementById("root")).render(
+        // pass the notes response from the axios.get method
+        <App notes={notes} />
+    );
+});
