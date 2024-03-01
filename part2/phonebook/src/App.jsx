46b26c3ac6f5d2c7d58e818c38b7741dccc0e93f
danutdotrar <morarasu.danut@gmail.com>
2024-03-01 12:37:14 +0200

    Add part2 phonebook ex2.8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,19 +1,23 @@
 import { useState } from "react";
 
 const App = () => {
-    const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+    const [persons, setPersons] = useState([
+        { name: "Arto Hellas", number: "420 6969" },
+    ]);
     const [newName, setNewName] = useState("");
+    const [newNumber, setNewNumber] = useState("");
 
     const handleSubmit = (e) => {
         e.preventDefault();
 
-        const newNameObj = {
+        const newPersonObj = {
             name: newName,
+            number: newNumber,
         };
 
         // verificam daca newName exista deja in person
         const personExists = persons.some(
-            (person) => person.name === newNameObj.name
+            (person) => person.name === newPersonObj.name
         );
 
         if (personExists) {
@@ -21,13 +25,18 @@ const App = () => {
         }
 
         if (personExists === false) {
-            setPersons(persons.concat(newNameObj));
+            setPersons(persons.concat(newPersonObj));
         }
         setNewName("");
+        setNewNumber("");
     };
 
-    const handleInputChange = (e) => {
+    const handleNameChange = (e) => {
         setNewName(e.target.value);
+    };
+
+    const handleNumberChange = (e) => {
+        setNewNumber(e.target.value);
     };
 
     return (
@@ -40,9 +49,19 @@ const App = () => {
                     <input
                         type="text"
                         value={newName}
-                        onChange={handleInputChange}
+                        onChange={handleNameChange}
                         name="name"
                         placeholder="Enter name..."
+                    />
+                </div>
+                <div className="form-container">
+                    <label htmlFor="number">Number: </label>
+                    <input
+                        type="text"
+                        value={newNumber}
+                        onChange={handleNumberChange}
+                        name="number"
+                        placeholder="Enter number..."
                     />
                 </div>
 
@@ -52,7 +71,9 @@ const App = () => {
             <div>
                 <h3>Numbers</h3>
                 {persons.map((person) => (
-                    <li>{person.name}</li>
+                    <li>
+                        {person.name} - {person.number}
+                    </li>
                 ))}
             </div>
         </>
