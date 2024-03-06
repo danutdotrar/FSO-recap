c41c19db628a8aa5e463422520bd5d605f1a12ea
danutdotrar <morarasu.danut@gmail.com>
2024-03-06 15:26:36 +0200

    Add part2 datacountries ex2.18

--- part2/datacountries/src/App.jsx
+++ part2/datacountries/src/App.jsx
@@ -1,35 +1,99 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [countries, setCountries] = useState(null);
+    const [countryName, setCountryName] = useState("");
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    useEffect(() => {
+        const baseURL = "https://studies.cs.helsinki.fi/restcountries/";
+
+        // connect to api
+        const fetchAllCountries = async () => {
+            // set countries state to all countries from api
+            const response = await axios.get(`${baseURL}api/all`);
+            setCountries(response.data);
+        };
+
+        fetchAllCountries();
+    }, [countryName]);
+
+    if (!countries) {
+        return null;
+    }
+
+    const handleSearch = (e) => {
+        const inputValue = e.target.value;
+        setCountryName(inputValue);
+    };
+
+    // filter countries, if > 10 return 'too many'
+    // if <= 10, render the names
+    // trebuie sa ne conectam la api/all
+    // sa rendam 'too many' daca sunt mai mult de 10 tari care includ input in nume.common
+    // daca sunt mai putine, rendam numele tarilor
+    // filtram countries cu countryName
+    // pastram tarile care includ countryName
+
+    const countriesFiltered = countries.filter((country) =>
+        country.name.common.toLowerCase().includes(countryName)
+    );
+
+    let content;
+
+    if (countriesFiltered.length > 10) {
+        content = "Too many, specify another filter";
+    }
+
+    if (countriesFiltered.length > 10 && countryName === "") {
+        content = "Enter smth";
+    }
+
+    if (countriesFiltered.length <= 10 && countriesFiltered.length > 1) {
+        content = countriesFiltered.map((country) => (
+            <li key={country.name.common}>{country.name.common}</li>
+        ));
+    }
+
+    if (countriesFiltered.length === 1) {
+        const singleCountry = countriesFiltered[0];
+        content = (
+            <>
+                <h3>{singleCountry.name.common}</h3>
+                <p>capital {singleCountry.capital}</p>
+                <p>area {singleCountry.area}</p>
+
+                <strong>languages:</strong>
+
+                {Object.entries(singleCountry.languages).map(([key, value]) => (
+                    <div key={key}>{value}</div>
+                ))}
+
+                <img src={singleCountry.flags.png} />
+            </>
+        );
+        console.log(countriesFiltered);
+    }
+
+    return (
+        <>
+            <div className="search-country">
+                find countries:{" "}
+                <input
+                    type="text"
+                    value={countryName}
+                    onChange={handleSearch}
+                />
+            </div>
+
+            <div className="render-countries">
+                {/* Render countries name that includes countryName */}
+                {content}
+            </div>
+        </>
+    );
 }
 
-export default App
+export default App;
