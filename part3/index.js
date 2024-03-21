8a760ac6ba24b542a61256af0de2db6de632ed97
danutdotrar <morarasu.danut@gmail.com>
2024-03-21 12:20:26 +0200

    Add mongodb settings in index.js

--- part3/index.js
+++ part3/index.js
@@ -8,6 +8,38 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+// MONGO DB
+// import mongoose
+const mongoose = require("mongoose");
+
+// access password
+const password = process.argv[2];
+
+// pass check
+if (process.argv.length < 3) {
+    console.log("give password as argument");
+    process.exit(1);
+}
+
+// define url
+const url = `mongodb+srv://morarasudanut:${password}@cluster0.qh6ymsj.mongodb.net/noteAppRecap?retryWrites=true&w=majority&appName=Cluster0`;
+
+mongoose.set("strictQuery", false);
+
+// connect to the url
+mongoose.connect(url);
+
+// define new mongoose Schema to use as mongoose model
+// the schema tells us how the content will be structured
+const noteSchema = new mongoose.Schema({
+    content: String,
+    important: Boolean,
+});
+
+// use note schema as model
+// models are constructor functions to create new JS object based on the schema
+const Note = mongoose.model("Note", noteSchema);
+
 // define sample API
 let notes = [
     {
@@ -33,7 +65,7 @@ let notes = [
 
 // GET '/api/notes"
 app.get("/api/notes", (request, response) => {
-    response.json(notes);
+    // Find all the notes in the mongoose database
 });
 
 // GET single resource by id '/api/notes/:id'
