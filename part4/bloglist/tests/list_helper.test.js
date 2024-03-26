1c5d6d864fe62ff5d19f5cd2e605762d1d5fd08b
danutdotrar <morarasu.danut@gmail.com>
2024-03-26 13:18:22 +0200

    Add part4a ex4.4

--- part4/bloglist/tests/list_helper.test.js
+++ part4/bloglist/tests/list_helper.test.js
@@ -10,3 +10,52 @@ test("dummy returns one", () => {
 
     assert.strictEqual(result, 1);
 });
+
+describe("total likes of ", () => {
+    test("empty list is zero", () => {
+        const blogs = [];
+
+        const result = listHelper.totalLikes(blogs);
+
+        assert.strictEqual(result, 0);
+    });
+
+    test("when list has one blog, return the likes of that", () => {
+        const listWithOneBlog = [
+            {
+                title: "test1",
+                author: "test1",
+                url: "test1",
+                likes: 12,
+                id: "66018d8a2b3c81aafdcc3a74",
+            },
+        ];
+
+        const result = listHelper.totalLikes(listWithOneBlog);
+
+        assert.strictEqual(result, 12);
+    });
+
+    test("a bigger list with blogs is done right", () => {
+        const listOfBlogs = [
+            {
+                title: "test1",
+                author: "test1",
+                url: "test1",
+                likes: 12,
+                id: "66018d8a2b3c81aafdcc3a74",
+            },
+            {
+                title: "test2",
+                author: "test2",
+                url: "test2",
+                likes: 10,
+                id: "66019801bb32238b612763ad",
+            },
+        ];
+
+        const result = listHelper.totalLikes(listOfBlogs);
+
+        assert.strictEqual(result, 22);
+    });
+});
--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -6,6 +6,14 @@ const dummy = (blogs) => {
 // return the total number of likes
 const totalLikes = (blogs) => {
     // blogs is an array of objects
+
+    // get all the likes
+    // const numOfLikes = blogs.map((blog) => blog.likes);
+
+    // reduce the numOfLikes array
+    const total = blogs.reduce((acc, val) => acc + val.likes, 0);
+
+    return blogs.length === 0 ? 0 : total;
 };
 
-module.exports = { dummy };
+module.exports = { dummy, totalLikes };
