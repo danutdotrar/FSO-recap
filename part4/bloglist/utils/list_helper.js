9c87ad49ce6c3a857197595ce598d26c039d1f38
danutdotrar <morarasu.danut@gmail.com>
2024-03-26 13:06:56 +0200

    Add part4a ex4.3

---
+++ part4/bloglist/utils/list_helper.js
@@ -0,0 +1,11 @@
+const dummy = (blogs) => {
+    //
+    return 1;
+};
+
+// return the total number of likes
+const totalLikes = (blogs) => {
+    // blogs is an array of objects
+};
+
+module.exports = { dummy };
