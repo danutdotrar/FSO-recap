769ad1b19b9224ef456d2350801efa33a1a74364
danutdotrar <morarasu.danut@gmail.com>
2024-04-23 14:01:32 +0300

    Add part5b ex5.7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,38 @@
-const Blog = ({ blog }) => (
-    <div>
-        {blog.title} {blog.author}
-    </div>
-);
+import { useState } from "react";
+
+const Blog = ({ blog }) => {
+    const [visibility, setVisibility] = useState(false);
+
+    const blogStyle = {
+        border: "1px solid black",
+        borderRadius: "5px",
+        margin: "5px",
+        padding: "5px",
+    };
+
+    const handleClick = () => {
+        setVisibility(!visibility);
+    };
+
+    const showWhenVisible = { display: visibility ? "" : "none" };
+    const hideWhenvisible = { display: visibility ? "none" : "" };
+
+    return (
+        <div style={blogStyle}>
+            {blog.title}{" "}
+            <button style={hideWhenvisible} onClick={handleClick}>
+                view
+            </button>
+            <button style={showWhenVisible} onClick={handleClick}>
+                hide
+            </button>
+            <div style={showWhenVisible}>
+                <div>url: {blog.url}</div>
+                <div>likes: {blog.likes}</div>
+                <div>author: {blog.author}</div>
+            </div>
+        </div>
+    );
+};
 
 export default Blog;
