07ddc7e2da1b5b5faa23cb2e1bae9d1940d99d29
danutdotrar <morarasu.danut@gmail.com>
2024-05-12 15:45:22 +0300

    Add part5 ex5.14

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -6,6 +6,8 @@
 // select the component by class name
 // expect that element to contain blog title and author
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
 import Blog from "./Blog";
 import { expect } from "vitest";
 
@@ -31,4 +33,36 @@ test("component displays only title and author at first render", () => {
     // check for togglable content to be display: none
     const togglableContent = container.querySelector(".togglableContent");
     expect(togglableContent).toHaveStyle("display: none;");
+});
+
+test("url and number of likes are shown when view is clicked", async () => {
+    // render the component with dummy data
+    // check for 'togglableContent' to be hidden (display: none)
+    // click on 'view' button
+    // check for 'togglableContent' to be shown (display not none)
+
+    // create dummy obj
+    const blog = {
+        title: "title",
+        author: "author",
+        url: "www",
+        likes: "3",
+        user: {
+            name: "name",
+        },
+    };
+
+    const { container } = render(<Blog blog={blog} />);
+    const togglableContent = container.querySelector(".togglableContent");
+
+    expect(togglableContent).toHaveStyle("display: none;");
+
+    // click 'view'
+    // start a user session
+    // await click
+    const viewBtn = screen.getByText("view");
+    const user = userEvent.setup();
+    await user.click(viewBtn);
+
+    expect(togglableContent).not.toHaveStyle("display: none;");
 });
