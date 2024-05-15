996028d1a7c0ced15a31d63a7bed648c6842e615
danutdotrar <morarasu.danut@gmail.com>
2024-05-15 12:09:00 +0300

    Add functionality to control DB state for testing

--- part4/courseinfo/app.js
+++ part4/courseinfo/app.js
@@ -45,6 +45,11 @@ app.use("/api/notes", notesRouter);
 app.use("/api/users", usersRouter);
 app.use("/api/login", loginRouter);
 
+if (process.env.NODE_ENV === "test") {
+    const testingRouter = require("../courseinfo/controllers/testing");
+    app.use("/api/testing", testingRouter);
+}
+
 // middlewares
 app.use(middleware.requestLogger);
 
