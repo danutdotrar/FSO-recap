e0f846bab6329b4bb5e96a333f52b9b1f11a1432
danutdotrar <morarasu.danut@gmail.com>
2024-05-20 14:21:28 +0300

    Add toggle_importance case based on test

---
+++ part6/courseinfo/src/reducers/noteReducer.js
@@ -0,0 +1,32 @@
+import { createStore } from "redux";
+
+const noteReducer = (state = [], action) => {
+    switch (action.type) {
+        case "NEW_NOTE":
+            return state.concat(action.payload);
+
+        case "TOGGLE_IMPORTANCE":
+            // search in the state the obj with the id equals to payload id
+            const noteToChange = state.find(
+                (item) => item.id === action.payload.id
+            );
+
+            // create a new object based on previous values but update only the important property
+            const changedNote = {
+                ...noteToChange,
+                important: !action.payload.important,
+            };
+
+            // map over the state and if id is different than keep the obj, if id === payload id than keep new obj
+            return state.map((item) =>
+                item.id !== action.payload.id ? item : changedNote
+            );
+
+        default:
+            return state;
+    }
+};
+
+const store = createStore(noteReducer);
+
+export { noteReducer };
