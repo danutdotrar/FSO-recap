c76637133aedccbdf33ec1ee72e76907a5f5af52
danutdotrar <morarasu.danut@gmail.com>
2024-05-21 14:04:22 +0300

    Refactor redux courseinfo a

--- part6/courseinfo/src/App.jsx
+++ part6/courseinfo/src/App.jsx
@@ -1,7 +1,21 @@
-import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { createNote, toggleImportanceOf } from "./reducers/noteReducer";
+import NewNote from "./components/NewNote";
+import Notes from "./components/Notes";
 
-function App() {
-    return <></>;
-}
+// App
+const App = () => {
+    const dispatch = useDispatch();
+    const notes = useSelector((state) => state);
+
+    return (
+        <>
+            <div>
+                <NewNote />
+                <Notes />
+            </div>
+        </>
+    );
+};
 
 export default App;
--- part6/courseinfo/src/main.jsx
+++ part6/courseinfo/src/main.jsx
@@ -2,22 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import { createStore } from "redux";
+import App from "./App";
 
 // // un reducer are ca parametrii state-ul curent si o actiune
 // // a reducer is a function with a state and a action as parameters, and has defined some actions that returns a new state
 // // a reducer need to be used as a parameter to createStore/configureStore to be used
-// const counterReducer = (state = 0, action) => {
-//     switch (action.type) {
-//         case "INCREMENT":
-//             return state + 1;
-//         case "DECREMENT":
-//             return state - 1;
-//         case "ZERO":
-//             return 0;
-//         default:
-//             return state;
-//     }
-// };
 
 // // reducer trebuie folosit ca parametru pentru createStore
 // // store-ul foloseste reducer-ul pentru a gestiona actiunile, care sunt trimise catre store cu metoda dispatch({type: 'nume actiune din reducer'})
@@ -30,88 +19,13 @@ import { createStore } from "redux";
 // //     console.log(storeNow);
 // // });
 
-// const App = () => {
-//     return (
-//         <div>
-//             <div>{store.getState()}</div>
-//             <button onClick={() => store.dispatch({ type: "INCREMENT" })}>
-//                 plus
-//             </button>
-
-//             <button onClick={() => store.dispatch({ type: "ZERO" })}>
-//                 zero
-//             </button>
-
-//             <button onClick={() => store.dispatch({ type: "DECREMENT" })}>
-//                 minus
-//             </button>
-//         </div>
-//     );
-// };
-
-import { noteReducer } from "./reducers/noteReducer";
+import noteReducer from "./reducers/noteReducer";
+import { Provider } from "react-redux";
 
 const store = createStore(noteReducer);
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-// App
-const generateId = () => Number((Math.random() * 1000000).toFixed(0));
-
-const App = () => {
-    const storeData = store.getState();
-
-    const addNote = (event) => {
-        event.preventDefault();
-
-        const content = event.target.note.value;
-        event.target.note.value = "";
-        store.dispatch({
-            type: "NEW_NOTE",
-            payload: {
-                content,
-                important: true,
-                id: generateId(),
-            },
-        });
-    };
-
-    const toggleImportance = (id) => {
-        store.dispatch({
-            type: "TOGGLE_IMPORTANCE",
-            payload: { id },
-        });
-    };
-
-    return (
-        <>
-            <div>
-                <form onSubmit={addNote}>
-                    <input name="note" />
-                    <button type="submit">add</button>
-                </form>
-                <ul>
-                    {storeData.map((note) => (
-                        <li
-                            key={note.id}
-                            onClick={() => toggleImportance(note.id)}
-                        >
-                            {note.content}{" "}
-                            {note.important ? " - important" : ""}
-                        </li>
-                    ))}
-                </ul>
-            </div>
-        </>
-    );
-};
-
-// create a function to render the app and use it in the store.subscribe
-const renderApp = () => {
-    root.render(<App />);
-};
-
-// first render of the app
-renderApp();
-// re-render the app everytime an action is sent to the store with the method store.dispatch({type: 'action name'})
-store.subscribe(renderApp);
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+);
