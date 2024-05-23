42b57fd3384130fed09c966820eac8eca3560757
danutdotrar <morarasu.danut@gmail.com>
2024-05-23 15:39:05 +0300

    Log anecdote id with redux

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,29 +1,50 @@
 const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
+    "If it hurts, do it more often",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+];
 
-const getId = () => (100000 * Math.random()).toFixed(0)
+const getId = () => (100000 * Math.random()).toFixed(0);
 
 const asObject = (anecdote) => {
-  return {
-    content: anecdote,
-    id: getId(),
-    votes: 0
-  }
-}
+    return {
+        content: anecdote,
+        id: getId(),
+        votes: 0,
+    };
+};
 
-const initialState = anecdotesAtStart.map(asObject)
+const initialState = anecdotesAtStart.map(asObject);
 
 const reducer = (state = initialState, action) => {
-  console.log('state now: ', state)
-  console.log('action', action)
+    console.log("state: ", state);
+    console.log("action: ", action);
 
-  return state
-}
+    switch (action.type) {
+        case "VOTE_ANECDOTE":
+            // take the id of the current anecdote
+            // find the anecdote we need to update in the state based on payload id
+            // create a copy based on anecdote and update votes + 1
+            // map over the state and for the items with id !== anecdote id, leave them alone
+            //  for the item with id == action.payload.id, replace with changed object
+            console.log("PAYLOAD ID - ", action.payload.id);
+            return state;
 
-export default reducer
+        default:
+            return state;
+    }
+};
+
+export const voteAnecdote = (id) => {
+    return {
+        type: "VOTE_ANECDOTE",
+        payload: {
+            id: id,
+        },
+    };
+};
+
+export default reducer;
