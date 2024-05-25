6e869875f9b8350db57bf6f1b8c6db41c52b6b55
danutdotrar <morarasu.danut@gmail.com>
2024-05-25 21:05:21 +0300

    Add part6b intro

--- part6/courseinfo/src/App.jsx
+++ part6/courseinfo/src/App.jsx
@@ -5,10 +5,40 @@ import Notes from "./components/Notes";
 
 // App
 const App = () => {
+    const filterSelected = (value) => {
+        console.log(value);
+    };
+
     return (
         <>
             <div>
                 <NewNote />
+                <div>
+                    all{" "}
+                    <input
+                        type="radio"
+                        name="filter"
+                        onChange={() => {
+                            filterSelected("ALL");
+                        }}
+                    />
+                    | important{" "}
+                    <input
+                        type="radio"
+                        name="filter"
+                        onChange={() => {
+                            filterSelected("IMPORTANT");
+                        }}
+                    />
+                    | non important{" "}
+                    <input
+                        type="radio"
+                        name="filter"
+                        onChange={() => {
+                            filterSelected("NONIMPORTANT");
+                        }}
+                    />
+                </div>
                 <Notes />
             </div>
         </>
