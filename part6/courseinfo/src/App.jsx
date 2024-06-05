3f43537d1c6667b0978aed714ea8e2b303b3b088
danutdotrar <morarasu.danut@gmail.com>
2024-06-05 14:49:00 +0300

    Remove some comments

--- part6/courseinfo/src/App.jsx
+++ part6/courseinfo/src/App.jsx
@@ -2,7 +2,6 @@ import { useSelector, useDispatch } from "react-redux";
 import NewNote from "./components/NewNote";
 import Notes from "./components/Notes";
 import VisibilityFilter from "./components/VisibilityFilter";
-import noteService from "./services/notes";
 import { useEffect } from "react";
 import { setNotes, initializeNotes } from "./reducers/noteReducer";
 
@@ -11,10 +10,6 @@ const App = () => {
 
     useEffect(() => {
         const fetchData = async () => {
-            // // HTTP request to get the notes
-            // const response = await noteService.getAll();
-            // // Populate the state
-            // dispatch(setNotes(response));
             dispatch(initializeNotes());
         };
         fetchData();
