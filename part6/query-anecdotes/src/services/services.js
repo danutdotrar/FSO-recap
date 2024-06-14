215d0c5173ba3a7a62703755dd11eb6da8707637
danutdotrar <morarasu.danut@gmail.com>
2024-06-14 15:32:03 +0300

    Add React Query starter settings

---
+++ part6/query-anecdotes/src/services/services.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const baseURL = "http://localhost:3001/anecdotes";
+
+// @@ GET request
+// @@ PATH baseURL
+// Get the anecdotes
+const getAll = async () => {
+    const response = await axios.get(baseURL);
+    return response.data;
+};
+
+export default {
+    getAll,
+};
