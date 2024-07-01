d8ac23e51bc9fc4938e2cd39cc3f2b3e87a616b8
danutdotrar <morarasu.danut@gmail.com>
2024-07-01 11:31:40 +0300

    Add Boostrap and style Notes

--- part7/courseinfo/src/App.jsx
+++ part7/courseinfo/src/App.jsx
@@ -1,5 +1,6 @@
 import ReactDOM from "react-dom/client";
 import { useState } from "react";
+import { Table } from "react-bootstrap";
 
 import {
     BrowserRouter as Router,
@@ -47,13 +48,18 @@ const Note = ({ note }) => {
 const Notes = ({ notes }) => (
     <div>
         <h2>Notes</h2>
-        <ul>
-            {notes.map((note) => (
-                <li key={note.id}>
-                    <Link to={`/notes/${note.id}`}>{note.content}</Link>
-                </li>
-            ))}
-        </ul>
+        <Table striped>
+            <tbody>
+                {notes.map((note) => (
+                    <tr key={note.id}>
+                        <td>
+                            <Link to={`/notes/${note.id}`}>{note.content}</Link>
+                        </td>
+                        <td>{note.user}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </Table>
     </div>
 );
 
@@ -133,7 +139,7 @@ const App = () => {
         : null;
 
     return (
-        <div>
+        <div className="container">
             <div>
                 <Link style={padding} to="/">
                     home
