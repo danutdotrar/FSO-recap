309404c07cd2d2733840620d71152553c705c7c5
danutdotrar <morarasu.danut@gmail.com>
2024-07-02 22:01:55 +0300

    Add webpack for courseinfo part7d

--- part7/webpack-courseinfo/webpack.config.js
+++ part7/webpack-courseinfo/webpack.config.js
@@ -7,6 +7,21 @@ const config = () => {
             path: path.resolve(__dirname, "build"),
             filename: "main.js",
         },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env", "@babel/preset-react"],
+                    },
+                },
+                {
+                    test: /\.css$/,
+                    use: ["style-loader", "css-loader"],
+                },
+            ],
+        },
     };
 };
 
