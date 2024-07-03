f733e11521e9701e44f33eb9e894d5c7fcfd2e39
danutdotrar <morarasu.danut@gmail.com>
2024-07-03 13:59:07 +0300

    Add webpack dev server

--- part7/webpack-courseinfo/src/App.js
+++ part7/webpack-courseinfo/src/App.js
@@ -1,8 +1,21 @@
-import React from "react"; // we need this now also in component files
+import React, { useState } from "react";
 import "./index.css";
 
 const App = () => {
-    return <div className="container">hello webpack</div>;
+    const [counter, setCounter] = useState(0);
+    const [values, setValues] = useState();
+
+    const handleClick = () => {
+        setCounter(counter + 1);
+        setValues(values.concat(counter));
+    };
+
+    return (
+        <div className="container">
+            hello webpack {counter} clicks
+            <button onClick={() => handleClick()}>press</button>
+        </div>
+    );
 };
 
 export default App;
