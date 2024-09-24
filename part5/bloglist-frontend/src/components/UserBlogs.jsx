35c1e9752b446cf0178606cd9c2fdd0b4a19e09f
danutdotrar <morarasu.danut@gmail.com>
2024-09-24 12:10:30 +0300

    Link each blog to its page

--- part5/bloglist-frontend/src/components/UserBlogs.jsx
+++ part5/bloglist-frontend/src/components/UserBlogs.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import usersService from "../services/users";
 
 const UserBlogs = () => {
@@ -25,7 +25,9 @@ const UserBlogs = () => {
             <>
                 <ul>
                     {userBlogs.map((blog) => (
-                        <li key={blog.title}>{blog.title}</li>
+                        <Link to={`/blogs/${blog.id}`} key={blog.id}>
+                            <li>{blog.title}</li>
+                        </Link>
                     ))}
                 </ul>
             </>
