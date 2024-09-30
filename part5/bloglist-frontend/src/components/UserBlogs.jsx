c473cda963840e78e5f7a55322277f700c264cb3
danutdotrar <morarasu.danut@gmail.com>
2024-09-30 11:48:37 +0300

    Add Togglable to UserBlogs

--- part5/bloglist-frontend/src/components/UserBlogs.jsx
+++ part5/bloglist-frontend/src/components/UserBlogs.jsx
@@ -1,11 +1,23 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import usersService from "../services/users";
+import Togglable from "./Togglable";
+import BlogForm from "./BlogForm";
+import { NotificationContext } from "../context/notificationContext";
+import { useQuery } from "@tanstack/react-query";
 
-const UserBlogs = () => {
+const UserBlogs = ({
+    handleBlogSubmit,
+    title,
+    author,
+    url,
+    setUrl,
+    blogFormRef,
+}) => {
     const params = useParams();
     const id = params.id;
 
+    const [state, dispatchState] = useContext(NotificationContext);
     const [userBlogs, setUserBlogs] = useState([]);
 
     useEffect(() => {
@@ -15,13 +27,35 @@ const UserBlogs = () => {
             setUserBlogs(userData.blogs);
         };
         getUserBlogs();
-    }, [id]);
+    }, [id, handleBlogSubmit]);
 
     if (!userBlogs) return <p>Loading...</p>;
 
     return (
         <div>
             <h2>added blogs</h2>
+
+            <Togglable ref={blogFormRef} buttonLabel="new blog">
+                <BlogForm
+                    handleBlogSubmit={handleBlogSubmit}
+                    title={title}
+                    author={author}
+                    url={url}
+                    handleTitle={(event) =>
+                        dispatchState({
+                            type: "SET_TITLE",
+                            payload: event.target.value,
+                        })
+                    }
+                    handleAuthor={(event) =>
+                        dispatchState({
+                            type: "SET_AUTHOR",
+                            payload: event.target.value,
+                        })
+                    }
+                    handleUrl={(event) => setUrl(event.target.value)}
+                />
+            </Togglable>
             <>
                 <ul>
                     {userBlogs.map((blog) => (
