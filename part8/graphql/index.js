df39db1e0feaebf26dc6b4e74ffff4a4d2259e4d
danutdotrar <morarasu.danut@gmail.com>
2024-10-10 13:27:09 +0300

    Add part8 courseinfo - GraphQL starter files

---
+++ part8/graphql/index.js
@@ -0,0 +1,52 @@
+// GraphQL is used for implementing the interfaces servers offers for browsers, and in general the integration between different applications on the web.
+// The main principle of GraphQL is that the code on the browsers form a 'query' describing the data we wanted, and sends it to the API with an HTTP POST request.
+// All GraphQL queries are POST requests
+// GraphQL needs a schema, which describes the data sent between the client and the server
+// Fields marked with exclamation mark must be given a value
+// Every GraphQL schema describes a Query, which tells what kind of queries can be made to the API
+// GraphQL describes only the data moving between the server and the client
+// On the database/server, the data can be organized in any way.
+
+const { ApolloServer } = require("@apollo/server");
+const { startStandaloneServer } = require("@apollo/server/standalone");
+
+// define persons array
+let persons = [
+    {
+        name: "Arto Hellas",
+        phone: "040-123543",
+        street: "Tapiolankatu 5 A",
+        city: "Espoo",
+        id: "3d594650-3436-11e9-bc57-8b80ba54c431",
+    },
+    {
+        name: "Matti Luukkainen",
+        phone: "040-432342",
+        street: "Malminkaari 10 A",
+        city: "Helsinki",
+        id: "3d599470-3436-11e9-bc57-8b80ba54c431",
+    },
+    {
+        name: "Venla Ruuska",
+        street: "Nallemäentie 22 C",
+        city: "Helsinki",
+        id: "3d599471-3436-11e9-bc57-8b80ba54c431",
+    },
+];
+
+// define typeDefs
+const typeDefs = `
+    type Person {
+        name: String!
+        phone: String
+        street: String!
+        city: String!
+        id: ID!
+    }
+
+    type Query {
+        personCount: Int!
+        allPersons: [Person]!
+        findPerson(name: String!): Person
+    }
+`;
