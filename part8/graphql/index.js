487abc22cb0eef763d5720dd4b1c2681f8371ff3
danutdotrar <morarasu.danut@gmail.com>
2024-10-11 18:06:56 +0300

    Filter persons by phone

--- part8/graphql/index.js
+++ part8/graphql/index.js
@@ -61,9 +61,14 @@ const typeDefs = `
         ): Person
     }
 
+    enum YesNo {
+        YES
+        NO
+    }
+
     type Query {
         personCount: Int!
-        allPersons: [Person]!
+        allPersons(phone: YesNo): [Person]!
         findPerson(name: String!): Person
     }
 `;
@@ -73,7 +78,16 @@ const typeDefs = `
 const resolvers = {
     Query: {
         personCount: () => persons.length,
-        allPersons: () => persons,
+        allPersons: (root, args) => {
+            if (!args.phone) {
+                return persons;
+            }
+
+            const byPhone = (person) =>
+                args.phone === "YES" ? person.phone : !person.phone;
+
+            return persons.filter(byPhone);
+        },
         findPerson: (root, args) =>
             persons.find((person) => person.name === args.name),
     },
