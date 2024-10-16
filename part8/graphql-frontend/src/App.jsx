bdad7857150afda1ce33c07b2cc21ad02c20914b
danutdotrar <morarasu.danut@gmail.com>
2024-10-16 13:18:16 +0300

    Make query with useQuery from App

--- part8/graphql-frontend/src/App.jsx
+++ part8/graphql-frontend/src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { gql, useQuery } from "@apollo/client";
+import "./App.css";
+import Persons from "./components/Persons";
+
+// define the query
+const ALL_PERSONS = gql`
+    query {
+        allPersons {
+            name
+            phone
+            id
+        }
+    }
+`;
 
 function App() {
-  const [count, setCount] = useState(0)
+    // useQuery will execute the query it receives as parameter
+    const result = useQuery(ALL_PERSONS);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    if (result.loading) {
+        return <div>Loading...</div>;
+    }
+
+    return <Persons persons={result.data.allPersons} />;
 }
 
-export default App
+export default App;
