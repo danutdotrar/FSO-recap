257b2f3ef580a76fd9d6ebe0dba080d524c010ca
danutdotrar <morarasu.danut@gmail.com>
2024-10-16 13:17:42 +0300

    Small refactor for main jsx

--- part8/graphql-frontend/src/main.jsx
+++ part8/graphql-frontend/src/main.jsx
@@ -1,10 +1,43 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import App from "./App.jsx";
+import "./index.css";
+import ReactDOM from "react-dom/client";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import {
+    ApolloClient,
+    ApolloProvider,
+    InMemoryCache,
+    gql,
+} from "@apollo/client";
+
+// create new client obj
+const client = new ApolloClient({
+    uri: "http://localhost:4000/",
+    cache: new InMemoryCache(),
+});
+
+// define the graphql query
+// const query = gql`
+//     query {
+//         allPersons {
+//             name
+//             phone
+//             address {
+//                 street
+//                 city
+//             }
+//             id
+//         }
+//     }
+// `;
+
+// the app can communicate with a GraphQL server using the `client` object
+// send query to the server
+// client.query({ query }).then((response) => {
+//     console.log(response.data);
+// });
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>
+);
