c8b53418e78e33e746cb2b79993b7da2431274d8
danutdotrar <morarasu.danut@gmail.com>
2024-10-24 13:03:40 +0300

    Add React Router and paths

--- part8/small-library-frontend/src/App.jsx
+++ part8/small-library-frontend/src/App.jsx
@@ -1,11 +1,21 @@
 import { useState } from "react";
 
 import "./App.css";
+import { Authors } from "./components/Authors";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./components/HomePage";
+import Navbar from "./components/Navbar";
 
 function App() {
     return (
         <>
-            <div>all authors here...</div>
+            <Router>
+                <Navbar />
+                <Routes>
+                    <Route path={"/"} element={<HomePage />} />
+                    <Route path={"/authors"} element={<Authors />} />
+                </Routes>
+            </Router>
         </>
     );
 }
