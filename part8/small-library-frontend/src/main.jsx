55e9700ecfb09d8c07068669799262d36026822e
danutdotrar <morarasu.danut@gmail.com>
2024-10-24 13:03:09 +0300

    Define Apollo Client & Provider

--- part8/small-library-frontend/src/main.jsx
+++ part8/small-library-frontend/src/main.jsx
@@ -2,12 +2,21 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import { ApolloClient, InMemoryCache, useQuery } from "@apollo/client";
+import { ApolloProvider } from "@apollo/client";
+import { gql } from "@apollo/client";
 
 // define apollo client
-// provide client to the app
+const client = new ApolloClient({
+    uri: "http://localhost:4000",
+    cache: new InMemoryCache(),
+});
 
+// provide client to the app
 createRoot(document.getElementById("root")).render(
     <StrictMode>
-        <App />
+        <ApolloProvider client={client}>
+            <App />
+        </ApolloProvider>
     </StrictMode>
 );
