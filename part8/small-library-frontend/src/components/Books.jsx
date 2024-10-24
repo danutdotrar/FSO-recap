3a416c07a164c12295b0e55ebeb45e30b2f93aac
danutdotrar <morarasu.danut@gmail.com>
2024-10-24 13:32:28 +0300

    Add Book component and GQL query

---
+++ part8/small-library-frontend/src/components/Books.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { useQuery } from "@apollo/client";
+import { ALL_BOOKS } from "../queries/queries";
+
+const Books = () => {
+    const books = useQuery(ALL_BOOKS);
+
+    if (books.loading) return <>Loading...</>;
+
+    return (
+        <div>
+            <h1>Books</h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th>author</th>
+                        <th>published</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {books.data.allBooks.map((book) => {
+                        return (
+                            <tr key={book.title}>
+                                <td style={{ textAlign: "start" }}>
+                                    {book.title}
+                                </td>
+                                <td>{book.author}</td>
+                                <td>{book.published}</td>
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </table>
+        </div>
+    );
+};
+
+export default Books;
