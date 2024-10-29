a2b8c29f8b718fd137690c0d2c3d18d23d7fbfb2
danutdotrar <morarasu.danut@gmail.com>
2024-10-29 11:47:42 +0200

    Add user model

---
+++ part8/graphql/models/user.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+// define the new mongoose schema
+const schema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        minLength: 3,
+    },
+    friends: [
+        {
+            type: mongoose.Schema.Types.ObjectID,
+            ref: "Person",
+        },
+    ],
+});
+
+// define the mongoose model
+const User = mongoose.model("User", schema);
+
+// export the model
+module.exports = User;
