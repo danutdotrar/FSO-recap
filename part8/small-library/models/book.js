f0680b32aa40426d4396b7ea76b9c136ed51644e
danutdotrar <morarasu.danut@gmail.com>
2024-10-30 12:56:20 +0200

    Add book model

---
+++ part8/small-library/models/book.js
@@ -0,0 +1,30 @@
+// import mongoose
+const mongoose = require("mongoose");
+
+const uniqueValidator = require("mongoose-unique-validator");
+
+// define the schema
+const bookSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        unique: true,
+        minlength: 5,
+    },
+    published: {
+        type: Number,
+    },
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Author",
+    },
+    genres: [{ type: String }],
+});
+
+bookSchema.plugin(uniqueValidator);
+
+// define the model
+const Book = mongoose.model("Book", bookSchema);
+
+// export the model
+module.exports = Book;
