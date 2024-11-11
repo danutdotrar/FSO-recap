5773f517362a5f9d608484296f0575b812dde154
danutdotrar <morarasu.danut@gmail.com>
2024-11-11 16:16:08 +0200

    Create http link to use the headers:authorization

--- part8/small-library-frontend/src/main.jsx
+++ part8/small-library-frontend/src/main.jsx
@@ -2,13 +2,37 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { ApolloClient, InMemoryCache } from "@apollo/client";
-import { ApolloProvider } from "@apollo/client";
+import {
+    ApolloClient,
+    InMemoryCache,
+    ApolloProvider,
+    createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+
+// define authLink
+const authLink = setContext((_, { headers }) => {
+    // get the token from localStorage
+    const token = localStorage.getItem("library-user-token");
+
+    return {
+        headers: {
+            ...headers,
+            authorization: token ? `Bearer ${token}` : null,
+        },
+    };
+});
+
+// createHttpLink
+const httpLink = createHttpLink({
+    uri: "http://localhost:4000",
+});
 
 // define apollo client
+// use link
 const client = new ApolloClient({
-    uri: "http://localhost:4000",
     cache: new InMemoryCache(),
+    link: authLink.concat(httpLink),
 });
 
 createRoot(document.getElementById("root")).render(
