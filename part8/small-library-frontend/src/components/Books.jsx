276bec2180ab8c28b02cf466e2fa7f84c0603e7b
danutdotrar <morarasu.danut@gmail.com>
2024-11-13 15:25:52 +0200

    Add part8d ex8.19

--- part8/small-library-frontend/src/components/Books.jsx
+++ part8/small-library-frontend/src/components/Books.jsx
@@ -1,11 +1,53 @@
 import React from "react";
 import { useQuery } from "@apollo/client";
 import { ALL_BOOKS } from "../queries/queries";
+import { useState } from "react";
 
 const Books = () => {
+    const [filteredGenre, setFilteredGenre] = useState("all genres");
+
     const books = useQuery(ALL_BOOKS);
 
     if (books.loading) return <>Loading...</>;
+
+    const allBooks = books.data.allBooks.filter((book) => {
+        // keep only the books that includes the genres of filteredGenre
+        return filteredGenre === "all genres"
+            ? book
+            : book.genres.includes(filteredGenre);
+    });
+
+    const genres = books.data.allBooks.map((book) => book.genres);
+
+    const getUniqueGenres = (array) => {
+        const obj = {};
+
+        for (const subarray of array) {
+            for (const item of subarray) {
+                // if the item exists, increment with 1
+                if (obj[item]) {
+                    obj[item] += 1;
+                }
+
+                // if the item doesnt exist, initialize with 1
+                if (!obj[item]) {
+                    obj[item] = 1;
+                }
+            }
+        }
+
+        return obj;
+    };
+
+    const uniqueGenres = [
+        "all genres",
+        ...Object.keys(getUniqueGenres(genres)),
+    ];
+
+    const handleButton = (e) => {
+        const genre = e.target.value;
+        setFilteredGenre(genre);
+    };
 
     return (
         <div>
@@ -19,7 +61,7 @@ const Books = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    {books.data.allBooks.map((book) => {
+                    {allBooks.map((book) => {
                         return (
                             <tr key={book.title + book.author.name}>
                                 <td style={{ textAlign: "start" }}>
@@ -32,6 +74,14 @@ const Books = () => {
                     })}
                 </tbody>
             </table>
+
+            <div>
+                {uniqueGenres.map((genre) => (
+                    <button value={genre} onClick={(e) => handleButton(e)}>
+                        {genre}
+                    </button>
+                ))}
+            </div>
         </div>
     );
 };
