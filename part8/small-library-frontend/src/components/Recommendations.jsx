45638bd6396cace4177f270d4390c303cf67189f
danutdotrar <morarasu.danut@gmail.com>
2024-11-15 14:09:22 +0200

    Add /recommendation view

---
+++ part8/small-library-frontend/src/components/Recommendations.jsx
@@ -0,0 +1,54 @@
+import { useQuery } from "@apollo/client";
+import React from "react";
+import { ALL_BOOKS, ME } from "../queries/queries";
+
+const Recommendations = () => {
+    // execute the 'me' query to get the details of the current user
+    // filter books by that genre
+    const user = useQuery(ME);
+    const books = useQuery(ALL_BOOKS);
+
+    const allBooks = books.data.allBooks;
+
+    const username = user?.data?.me?.username;
+    const favoriteGenre = user?.data?.me?.favoriteGenre;
+
+    const filteredBooks = allBooks.filter((book) =>
+        book.genres.includes(favoriteGenre)
+    );
+
+    return (
+        <>
+            <h1>Recommendations</h1>
+            <div>
+                <p>hello, {username}!</p>
+                <p>books in your favorite genre: {favoriteGenre}</p>
+            </div>
+
+            <table>
+                <thead>
+                    <tr>
+                        <td></td>
+                        <td>
+                            <b>author</b>
+                        </td>
+                        <td>
+                            <b>published</b>
+                        </td>
+                    </tr>
+                </thead>
+                <tbody>
+                    {filteredBooks.map((book) => (
+                        <tr>
+                            <td>{book.title}</td>
+                            <td>{book.author.name}</td>
+                            <td>{book.published}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </>
+    );
+};
+
+export default Recommendations;
