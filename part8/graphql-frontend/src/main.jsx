629099c61e99e0344ccf0feb8ba4f451b7364ef9
danutdotrar <morarasu.danut@gmail.com>
2024-11-29 18:05:26 +0200

    Add ws and splitlink

--- part8/graphql-frontend/src/main.jsx
+++ part8/graphql-frontend/src/main.jsx
@@ -7,8 +7,13 @@ import {
     ApolloProvider,
     InMemoryCache,
     createHttpLink,
+    split,
 } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
+
+import { getMainDefinition } from "@apollo/client/utilities";
+import { GraphQLWsLink } from "@apollo/client/link/subscriptions";
+import { createClient } from "graphql-ws";
 
 // define authLink
 const authLink = setContext((_, { headers }) => {
@@ -27,10 +32,24 @@ const httpLink = createHttpLink({
     uri: "http://localhost:4000",
 });
 
+const wsLink = new GraphQLWsLink(createClient({ url: "ws://localhost:4000" }));
+
+const splitLink = split(
+    ({ query }) => {
+        const definition = getMainDefinition(query);
+        return (
+            definition.kind === "OperationDefinition" &&
+            definition.operation === "subscription"
+        );
+    },
+    wsLink,
+    authLink.concat(httpLink)
+);
+
 // create new client obj
 const client = new ApolloClient({
     cache: new InMemoryCache(),
-    link: authLink.concat(httpLink),
+    link: splitLink,
 });
 
 ReactDOM.createRoot(document.getElementById("root")).render(
