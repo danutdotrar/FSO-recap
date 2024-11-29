cb262fd2238708e44346bb2dafa9c18d0559ddbe
danutdotrar <morarasu.danut@gmail.com>
2024-11-29 18:06:39 +0200

    useSubscription to update cache

--- part8/graphql-frontend/src/App.jsx
+++ part8/graphql-frontend/src/App.jsx
@@ -1,18 +1,58 @@
 import { useState } from "react";
-import { gql, useQuery, useApolloClient } from "@apollo/client";
+import { useQuery, useApolloClient, useSubscription } from "@apollo/client";
 import "./App.css";
 import Persons from "./components/Persons";
 import PersonForm from "./components/PersonForm";
-import { ALL_PERSONS } from "./queries/queries.js";
+import { ALL_PERSONS, PERSON_ADDED } from "./queries/queries.js";
 import { Notify } from "./components/Notify";
 import PhoneForm from "./components/PhoneForm";
 import LoginForm from "./components/LoginForm";
+
+// taking care of manipulating cache
+export const updateCache = (cache, query, addedPerson) => {
+    // helper, eliminate saving same person twice
+    const uniqByName = (a) => {
+        // define new set
+        let seen = new Set();
+
+        // filter the array
+        return a.filter((item) => {
+            // if the set has current item then skip it
+            // if set has NOT current item then add it to the set
+            // seen.add is truthy so it'll keep it in filtered arr
+            return seen.has(item.name) ? false : seen.add(item.name);
+        });
+    };
+
+    cache.updateQuery(query, ({ allPersons }) => {
+        return {
+            allPersons: uniqByName(allPersons.concat(addedPerson)),
+        };
+    });
+};
 
 function App() {
     const [errorMessage, setErrorMessage] = useState(null);
     const [token, setToken] = useState(null);
     const result = useQuery(ALL_PERSONS);
     const client = useApolloClient();
+
+    useSubscription(PERSON_ADDED, {
+        onData: ({ data, client }) => {
+            const addedPerson = data.data.personAdded;
+
+            updateCache(client.cache, { query: ALL_PERSONS }, addedPerson);
+
+            // client.cache.updateQuery(
+            //     { query: ALL_PERSONS },
+            //     ({ allPersons }) => {
+            //         return {
+            //             allPersons: allPersons.concat(addedPerson),
+            //         };
+            //     }
+            // );
+        },
+    });
 
     // useQuery will execute the query it receives as parameter
     if (result.loading) {
