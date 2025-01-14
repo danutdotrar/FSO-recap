33284a64cefd359f3c965760a8e560caece1fceb
danutdotrar <morarasu.danut@gmail.com>
2025-01-14 16:02:13 +0200

    Add schema to its own moduel

---
+++ part8/small-library/schema/schema.js
@@ -0,0 +1,64 @@
+// define the typeDefs schema
+const typeDefs = `
+type Query {
+    bookCount: Int!
+    authorCount: Int!
+    allBooks(author: String, genre: String): [Book!]!
+    allAuthors: [Author!]!
+    findBook(genre: String): [Book!]!
+    me: User
+}
+
+type User {
+    username: String!
+    favoriteGenre: String!
+}
+
+type Token {
+    value: String!
+}
+
+type Book {
+    title: String!
+    published: Int!
+    author: Author!
+    genres: [String!]!
+    id: ID!
+}
+
+type Author {
+    name: String!
+    born: Int
+    bookCount: Int
+}
+
+type Mutation {
+    addBook(
+        title: String!
+        author: String!
+        published: Int!
+        genres: [String!]!
+        ) : Book!
+        
+    editAuthor(
+        name: String
+        setBornTo: Int
+        ) : Author
+
+    createUser(
+        username: String!
+        favoriteGenre: String!
+        ) : User
+
+    login(
+        username: String!
+        password: String!
+        ) : Token
+    }
+
+    type Subscription {
+        bookAdded: Book!
+    }
+        `;
+
+module.exports = typeDefs;
