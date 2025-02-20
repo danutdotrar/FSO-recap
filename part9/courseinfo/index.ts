f8fe21583ab5dc785fdf0e528a17ed4c744a0beb
danutdotrar <morarasu.danut@gmail.com>
2025-02-20 18:15:41 +0200

    POST req for calculator

--- part9/courseinfo/index.ts
+++ part9/courseinfo/index.ts
@@ -1,6 +1,9 @@
 // const express = require("express");
 import express from "express";
 const app = express();
+app.use(express.json());
+
+import { calculator, Operation } from "./calculator";
 
 app.get("/ping", (_req, res) => {
     res.send("pong");
@@ -8,6 +11,20 @@ app.get("/ping", (_req, res) => {
 
 app.get("/", (_req, res) => {
     res.send("this is the home page");
+});
+
+app.post("/calculate", (req, res) => {
+    const { value1, value2, op } = req.body;
+
+    if (!value1 || !isNaN(value1) || !value2 || !isNaN(value2)) {
+        res.status(400).send({ error: "bad values" });
+    }
+
+    // type assertion
+    const operation = op as Operation;
+
+    const result = calculator(Number(value1), Number(value2), operation);
+    res.send({ result });
 });
 
 const PORT = 3000;
