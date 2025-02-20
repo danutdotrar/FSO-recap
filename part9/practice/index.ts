c4cc124e41c528a686b680e8822f77f4e717ee90
danutdotrar <morarasu.danut@gmail.com>
2025-02-20 18:16:16 +0200

    Add error fix alternative

--- part9/practice/index.ts
+++ part9/practice/index.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from "express";
 const app = express();
+app.use(express.json());
 
 import { calculateBmi } from "./bmiCalculator";
 
@@ -7,26 +8,40 @@ app.get("/endpoint", (_req, res) => {
     res.send("Hello Full Stack!");
 });
 
-app.get("/bmi", (req: Request, res: Response) => {
+// app.get("/bmi", (req: Request, res: Response) => {
+//     const params = req.query;
+
+//     const height: number = Number(params.height);
+//     const weight: number = Number(params.weight);
+
+//     if (isNaN(height) || isNaN(weight)) {
+//         res.status(400).json({ error: "malformatted parameters" });
+//     }
+
+//     try {
+//         const bmiResult: string = calculateBmi(height, weight);
+//         res.json({ height, weight, bmi: bmiResult });
+//     } catch (error: unknown) {
+//         let errorMessage = "error: ";
+//         if (error instanceof Error) {
+//             errorMessage += error.message;
+//         }
+//         res.json({ error: errorMessage });
+//     }
+// });
+
+app.get("/bmi", (req, res) => {
     const params = req.query;
 
     const height: number = Number(params.height);
     const weight: number = Number(params.weight);
 
     if (isNaN(height) || isNaN(weight)) {
-        res.status(400).json({ error: "malformatted parameters" });
+        res.status(400).send({ error: "malformatted parameters" });
     }
 
-    try {
-        const bmiResult: string = calculateBmi(height, weight);
-        res.json({ height, weight, bmi: bmiResult });
-    } catch (error: unknown) {
-        let errorMessage = "error: ";
-        if (error instanceof Error) {
-            errorMessage += error.message;
-        }
-        res.json({ error: errorMessage });
-    }
+    const bmiResult: string = calculateBmi(height, weight);
+    res.send({ height, weight, bmi: bmiResult });
 });
 
 const PORT = 3000;
