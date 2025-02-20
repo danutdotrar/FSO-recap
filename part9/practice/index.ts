2d1d20a8a0a32fc26ae14f4d99d6999c07dcdda9
danutdotrar <morarasu.danut@gmail.com>
2025-02-20 17:50:30 +0200

    Add part9b exercise 9.5

--- part9/practice/index.ts
+++ part9/practice/index.ts
@@ -1,8 +1,32 @@
-import express from "express";
+import express, { Request, Response } from "express";
 const app = express();
+
+import { calculateBmi } from "./bmiCalculator";
 
 app.get("/endpoint", (_req, res) => {
     res.send("Hello Full Stack!");
+});
+
+app.get("/bmi", (req: Request, res: Response) => {
+    const params = req.query;
+
+    const height: number = Number(params.height);
+    const weight: number = Number(params.weight);
+
+    if (isNaN(height) || isNaN(weight)) {
+        res.status(400).json({ error: "malformatted parameters" });
+    }
+
+    try {
+        const bmiResult: string = calculateBmi(height, weight);
+        res.json({ height, weight, bmi: bmiResult });
+    } catch (error: unknown) {
+        let errorMessage = "error: ";
+        if (error instanceof Error) {
+            errorMessage += error.message;
+        }
+        res.json({ error: errorMessage });
+    }
 });
 
 const PORT = 3000;
