60062fa4c09e1922f787ab997ff5933284665fcd
danutdotrar <morarasu.danut@gmail.com>
2025-03-03 18:26:38 +0200

    Add routes and services

---
+++ part9/flight-diary/src/services/diaryService.ts
@@ -0,0 +1,12 @@
+import diaries from "../../data/diaryentries";
+import { DiaryEntry } from "../../types";
+
+const getEntries = (): DiaryEntry[] => {
+    return diaries;
+};
+
+const addDiary = () => {
+    return null;
+};
+
+export default { getEntries, addDiary };
