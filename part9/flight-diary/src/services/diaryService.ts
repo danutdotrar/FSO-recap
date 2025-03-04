a5c6f434c69276345d4f2331fa5e065d03a989a6
danutdotrar <morarasu.danut@gmail.com>
2025-03-04 18:39:37 +0200

    Get non sensitive entries

--- part9/flight-diary/src/services/diaryService.ts
+++ part9/flight-diary/src/services/diaryService.ts
@@ -1,12 +1,21 @@
 import diaries from "../../data/diaryentries";
-import { DiaryEntry } from "../../types";
+import { DiaryEntry, NonSensitiveDiaryEntry } from "../types";
 
 const getEntries = (): DiaryEntry[] => {
     return diaries;
+};
+
+const getNonSensitiveEntries = (): NonSensitiveDiaryEntry[] => {
+    return diaries.map(({ id, date, weather, visibility }) => ({
+        id,
+        date,
+        weather,
+        visibility,
+    }));
 };
 
 const addDiary = () => {
     return null;
 };
 
-export default { getEntries, addDiary };
+export default { getEntries, addDiary, getNonSensitiveEntries };
