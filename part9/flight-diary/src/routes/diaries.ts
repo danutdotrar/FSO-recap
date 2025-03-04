0681f970bf263339a2220b48af7f285001b9d57a
danutdotrar <morarasu.danut@gmail.com>
2025-03-04 18:47:49 +0200

    Define response type

--- part9/flight-diary/src/routes/diaries.ts
+++ part9/flight-diary/src/routes/diaries.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import diaryService from "../services/diaryService";
+import { Response } from "express";
+import { NonSensitiveDiaryEntry } from "../types";
 
 const router = express.Router();
 
-router.get("/", (_req, res) => {
+router.get("/", (_req, res: Response<NonSensitiveDiaryEntry[]>) => {
     res.send(diaryService.getNonSensitiveEntries());
 });
 
