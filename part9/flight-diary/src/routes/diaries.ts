5f3ddc4ea606a0708e7e2467f1d732a3f95585a2
danutdotrar <morarasu.danut@gmail.com>
2025-03-05 17:50:11 +0200

    Fetch single resource and POST resource

--- part9/flight-diary/src/routes/diaries.ts
+++ part9/flight-diary/src/routes/diaries.ts
@@ -9,8 +9,36 @@ router.get("/", (_req, res: Response<NonSensitiveDiaryEntry[]>) => {
     res.send(diaryService.getNonSensitiveEntries());
 });
 
-router.post("/", (_req, res) => {
-    res.send("Saving a diary");
+router.get("/:id", (req, res) => {
+    const id: number = Number(req.params.id);
+
+    const diary = diaryService.findById(id);
+
+    try {
+        res.send(diary);
+    } catch (error: unknown) {
+        let errorMessage = "Something bad happened";
+
+        // narrowing error to access 'message' property
+        if (error instanceof Error) {
+            errorMessage += ` :${error.message}`;
+        }
+
+        res.sendStatus(400).send(errorMessage);
+    }
+});
+
+router.post("/", (req, res) => {
+    const { date, weather, visibility, comment } = req.body;
+
+    const addedEntry = diaryService.addDiary({
+        date,
+        weather,
+        visibility,
+        comment,
+    });
+
+    res.json(addedEntry);
 });
 
 export default router;
