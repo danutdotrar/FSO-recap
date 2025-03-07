97d50cc8f4556b6dab4e36f75102f2a407f45c9c
danutdotrar <morarasu.danut@gmail.com>
2025-03-07 15:42:01 +0200

    Refactor Visibility into Enum Type

--- part9/flight-diary/src/types.ts
+++ part9/flight-diary/src/types.ts
@@ -1,5 +1,3 @@
-// export type Weather = "sunny" | "rainy" | "cloudy" | "windy" | "stormy";
-
 export enum Weather {
     Sunny = "sunny",
     Rainy = "rainy",
@@ -8,7 +6,12 @@ export enum Weather {
     Windy = "windy",
 }
 
-export type Visibility = "great" | "good" | "ok" | "poor";
+export enum Visibility {
+    Great = "great",
+    Good = "good",
+    Ok = "ok",
+    Poor = "poor",
+}
 
 export type NonSensitiveDiaryEntry = Omit<DiaryEntry, "comment">;
 
