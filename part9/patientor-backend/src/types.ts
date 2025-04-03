a2cdd603f348e06295cf5a2ea40a5359bf82cd11
danutdotrar <morarasu.danut@gmail.com>
2025-04-03 15:45:16 +0300

    Add gender enum type

--- part9/patientor-backend/src/types.ts
+++ part9/patientor-backend/src/types.ts
@@ -7,14 +7,20 @@ export interface Patients {
     occupation: string;
 }
 
+export enum Gender {
+    Male = "male",
+    Female = "female",
+    Other = "other",
+}
+
 // TypeScript only checks if we have all the required fields or not, but excess fields are not prohibited
 // TypeScript doens't modify the actaul data. We need to exclude the fields ourselves
 export type NonSensitivePatients = Omit<Patients, "ssn">;
+
+export type NewPatientEntry = Omit<Patients, "id">;
 
 export interface Diagnosis {
     code: string;
     name: string;
     latin?: string;
 }
-
-export type NewPatientEntry = Omit<Patients, "id">;
