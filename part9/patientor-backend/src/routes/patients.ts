4719c844c51be190d6ab43753f256b947ba54e6d
danutdotrar <morarasu.danut@gmail.com>
2025-05-02 17:49:38 +0300

    POST new patient

--- part9/patientor-backend/src/routes/patients.ts
+++ part9/patientor-backend/src/routes/patients.ts
@@ -2,6 +2,7 @@ import express, { Response } from "express";
 const router = express.Router();
 import patientsService from "../services/patients";
 import { NonSensitivePatients } from "../types";
+import toNewPatientEntry from "../utils";
 
 router.get("/", (_req, res: Response<NonSensitivePatients[]>) => {
     res.send(patientsService.getNonSensitivePatients());
@@ -10,10 +11,22 @@ router.get("/", (_req, res: Response<NonSensitivePatients[]>) => {
 // POST request
 // Add patients to patients.ts 'db'
 // route '/'
-router.post("/", (req, res: Response<NonSensitivePatients>) => {
-    const body = req.body;
+router.post("/", (req, res) => {
+    try {
+        const body = req.body;
 
-    res.send(patientsService.addNewPatientEntry(body));
+        const newPatient = toNewPatientEntry(body);
+
+        res.send(patientsService.addNewPatientEntry(newPatient));
+    } catch (error: unknown) {
+        let errorMesasge = "Something went wrong.";
+
+        if (error instanceof Error) {
+            errorMesasge += " Error: " + error.message;
+        }
+
+        res.status(404).send(errorMesasge);
+    }
 });
 
 export default router;
