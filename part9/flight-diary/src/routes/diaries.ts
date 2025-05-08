87fd7d0ebdc2d7db7e40d9bf3195decd15080491
danutdotrar <morarasu.danut@gmail.com>
2025-05-08 16:33:59 +0300

    Add POST error handling

--- part9/flight-diary/src/routes/diaries.ts
+++ part9/flight-diary/src/routes/diaries.ts
@@ -3,6 +3,7 @@ import diaryService from "../services/diaryService";
 import { Response } from "express";
 import { NonSensitiveDiaryEntry } from "../types";
 import toNewDiaryEntry from "../utils";
+import { z } from "zod";
 
 const router = express.Router();
 
@@ -30,10 +31,18 @@ router.get("/:id", (req, res) => {
 });
 
 router.post("/", (req, res) => {
-    const newDiaryEntry = toNewDiaryEntry(req.body);
-    const addedEntry = diaryService.addDiary(newDiaryEntry);
+    try {
+        const newDiaryEntry = toNewDiaryEntry(req.body);
+        const addedEntry = diaryService.addDiary(newDiaryEntry);
 
-    res.json(addedEntry);
+        res.json(addedEntry);
+    } catch (error: unknown) {
+        if (error instanceof z.ZodError) {
+            res.status(400).send({ error: error.issues });
+        } else {
+            res.status(400).send({ error: "unknown error" });
+        }
+    }
 });
 
 export default router;
