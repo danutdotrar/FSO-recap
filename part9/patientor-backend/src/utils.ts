4f6c167c39f47cc2aea644ac9992d5b6b05b30b7
danutdotrar <morarasu.danut@gmail.com>
2025-05-09 16:30:22 +0300

    Install zod and validate new patient

--- part9/patientor-backend/src/utils.ts
+++ part9/patientor-backend/src/utils.ts
@@ -1,86 +1,72 @@
+import { z } from "zod";
 import { Gender, NewPatientEntry } from "./types";
 
 // type guard
-const isString = (text: unknown): text is string => {
-    return typeof text === "string" || text instanceof String;
-};
+// const isString = (text: unknown): text is string => {
+//     return typeof text === "string" || text instanceof String;
+// };
 
-const parseName = (name: unknown): string => {
-    if (!name || !isString(name)) {
-        throw new Error("Invalid or missing data: " + name);
-    }
+// const parseName = (name: unknown): string => {
+//     if (!name || !isString(name)) {
+//         throw new Error("Invalid or missing data: " + name);
+//     }
 
-    return name;
-};
+//     return name;
+// };
 
-const isDate = (date: string): boolean => {
-    return Boolean(Date.parse(date));
-};
+// const isDate = (date: string): boolean => {
+//     return Boolean(Date.parse(date));
+// };
+// const parseDate = (date: unknown): string => {
+//     if (!date || !isString(date) || !isDate(date)) {
+//         throw new Error("Invalid or missing data: " + date);
+//     }
 
-const parseDate = (date: unknown): string => {
-    if (!date || !isString(date) || !isDate(date)) {
-        throw new Error("Invalid or missing data: " + date);
-    }
+//     return date;
+// };
 
-    return date;
-};
+// const parseSsn = (ssn: unknown): string => {
+//     if (!ssn || !isString(ssn)) {
+//         throw new Error("Invalid or missing data: " + ssn);
+//     }
 
-const parseSsn = (ssn: unknown): string => {
-    if (!ssn || !isString(ssn)) {
-        throw new Error("Invalid or missing data: " + ssn);
-    }
+//     return ssn;
+// };
 
-    return ssn;
-};
+// const isGender = (gender: string): gender is Gender => {
+//     const isValidGender = Object.values(Gender)
+//         .map((item) => item.toString())
+//         .includes(gender);
 
-const isGender = (gender: string): gender is Gender => {
-    const isValidGender = Object.values(Gender)
-        .map((item) => item.toString())
-        .includes(gender);
+//     return isValidGender;
+// };
+// const parseGender = (gender: unknown): Gender => {
+//     if (!gender || !isString(gender) || !isGender(gender)) {
+//         throw new Error("Invalid or missing data: " + gender);
+//     }
 
-    return isValidGender;
-};
+//     return gender;
+// };
 
-const parseGender = (gender: unknown): Gender => {
-    if (!gender || !isString(gender) || !isGender(gender)) {
-        throw new Error("Invalid or missing data: " + gender);
-    }
+// const parseOccupation = (occupation: unknown): string => {
+//     if (!occupation || !isString(occupation)) {
+//         throw new Error("Invalid or missing data: " + occupation);
+//     }
 
-    return gender;
-};
+//     return occupation;
+// };
 
-const parseOccupation = (occupation: unknown): string => {
-    if (!occupation || !isString(occupation)) {
-        throw new Error("Invalid or missing data: " + occupation);
-    }
-
-    return occupation;
-};
+// refactor to use zod object schema
+const newPatientSchema = z.object({
+    name: z.string(),
+    dateOfBirth: z.string().date(),
+    ssn: z.string(),
+    gender: z.nativeEnum(Gender),
+    occupation: z.string(),
+});
 
 const toNewPatientEntry = (entry: unknown): NewPatientEntry => {
-    if (!entry || typeof entry != "object") {
-        throw new Error("Invalid or missing data.");
-    }
-
-    if (
-        "name" in entry &&
-        "dateOfBirth" in entry &&
-        "ssn" in entry &&
-        "gender" in entry &&
-        "occupation" in entry
-    ) {
-        const newEntry: NewPatientEntry = {
-            name: parseName(entry.name),
-            dateOfBirth: parseDate(entry.dateOfBirth),
-            ssn: parseSsn(entry.ssn),
-            gender: parseGender(entry.gender),
-            occupation: parseOccupation(entry.occupation),
-        };
-
-        return newEntry;
-    }
-
-    throw new Error("Invalid entry. Some fields are missing.");
+    return newPatientSchema.parse(entry);
 };
 
 export default toNewPatientEntry;
