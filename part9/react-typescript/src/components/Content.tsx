7dc1d0c673df3e0199be0ff41ccedd6667d6dd17
danutdotrar <morarasu.danut@gmail.com>
2025-05-12 14:27:58 +0300

    Add part9 ex9.16

--- part9/react-typescript/src/components/Content.tsx
+++ part9/react-typescript/src/components/Content.tsx
@@ -1,19 +1,79 @@
-interface ContentPart {
-    name: string;
-    exerciseCount: number;
-}
+import type { CoursePart } from "../App";
 
 interface ContentProps {
-    courseParts: ContentPart[];
+    courseParts: CoursePart[];
 }
 
-const Content = (props: ContentProps) => {
+interface PartProps {
+    part: CoursePart;
+    index: number;
+}
+
+// if we add a new type in a union/extend, exhaustive type checking will tell us if we also added it in the switch case
+// used for checking if all the cases from a discriminated union are covered (a sum of types with a common field - 'kind')
+// if a type that exists in the type union but it's not covered/user, then the assertNever will throw an error
+const assertNever = (value: never): never => {
+    throw new Error(
+        `Unhandled discriminated union member: ${JSON.stringify(value)}`
+    );
+};
+
+const Part = ({ part, index }: PartProps) => {
+    switch (part.kind) {
+        case "basic":
+            return (
+                <div key={index}>
+                    <h3>{part.name}</h3>
+                    <p>{part.description}</p>
+                    <p>Exercises: {part.exerciseCount}</p>
+                </div>
+            );
+            break;
+
+        case "group":
+            return (
+                <div key={index}>
+                    <h3>{part.name}</h3>
+                    <p>Group projects: {part.groupProjectCount}</p>
+                    <p>Exercises: {part.exerciseCount}</p>
+                </div>
+            );
+            break;
+
+        case "background":
+            return (
+                <div key={index}>
+                    <h3>{part.name}</h3>
+                    <p>{part.description}</p>
+                    <a href={part.backgroundMaterial}>Material</a>
+                    <p>Exercises: {part.exerciseCount}</p>
+                </div>
+            );
+            break;
+
+        case "special":
+            return (
+                <div key={index}>
+                    <h3>{part.name}</h3>
+                    <p>{part.description}</p>
+                    <p>Requirments: {part.requirements.join(", ")}</p>
+                    <p>Exercises: {part.exerciseCount}</p>
+                </div>
+            );
+            break;
+
+        default:
+            // check if all cases are covered
+            return assertNever(part);
+            break;
+    }
+};
+
+const Content = ({ courseParts }: ContentProps) => {
     return (
         <>
-            {props.courseParts.map((item) => (
-                <p>
-                    {item.name} {item.exerciseCount}
-                </p>
+            {courseParts.map((part, index) => (
+                <Part part={part} index={index} />
             ))}
         </>
     );
